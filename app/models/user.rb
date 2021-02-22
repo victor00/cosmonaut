202e09d04bfeb1ee590bf67727c6c4432237fed4
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-22 19:27:22 -0300

    trip model validations and some minor validations changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :trips
+  has_many :tickets, dependent: :destroy
+  after_destroy :cleanup
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -10,4 +12,21 @@ class User < ApplicationRecord
             presence: true
 
   validates :origin_planet, :email, uniqueness: true
+
+  validates :password, confirmation: true,
+                       unless: proc { |a| a.password.blank? }
+
+  private
+
+  def cleanup
+    if pilot
+      trips.destroy_all
+    else
+      trips.destroy
+    end
+  end
+
+  def pilot?
+    pilot
+  end
 end
