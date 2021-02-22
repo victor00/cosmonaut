fb0e9a6c119ec98d1fa9ab26986888e6c10c41e8
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-22 18:53:12 -0300

    trip model added

---
+++ db/migrate/20210222215202_create_trips.rb
@@ -0,0 +1,15 @@
+class CreateTrips < ActiveRecord::Migration[6.0]
+  def change
+    create_table :trips do |t|
+      t.string :origin, null: false
+      t.string :destination, null: false
+      t.date :launch_date, null: false
+      t.float :price, null: false
+      t.integer :max_tripulation, null: false
+      t.string :spaceship_name, null: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
