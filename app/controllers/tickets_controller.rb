57df4b6106736a7e22eb23e793e4dabb33190bbd
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-23 14:54:11 -0300

    buy_a_trip action implemented

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,24 +1,21 @@
 class TicketsController < ApplicationController
-  def def new
-    @ticket = Ticket.new
-  end
-
   def show
     @ticket = Ticket.find(params[:id])
   end
 
   def create
+    @trip = Trip.find(params[:trip_id])
     @ticket = Ticket.new(ticket_params)
+    @ticket.user = current_user
+    @ticket.trip = @trip
     if @ticket.save
       redirect_to ticket_path(@ticket)
     else
-      render :new
+      render "trips/show"
     end
   end
 
-  private
-
   def ticket_params
-    params.require(:ticket).permit(:user, :trip)
+    params.require(:ticket).permit(:class_option)
   end
 end
