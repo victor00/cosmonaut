98ca15eb1b2fff995ae7c7d44d82a21340cdeed3
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-23 11:46:46 -0300

    home navbar working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
   def home
   end
-
-  spaceship_names = [
-    "tardis", "bebop", "apollo", "ares", "nimbus", "x-wing",
-    "destroyer", "millennium falcon", "galactic empire", "space cruiser",
-    "discovery", "starship enterprise", "U.S.S enterprise"
-  ]
-  planets = [
-    "Venzenulon 9", "Gazorpazorp", "Cromulon", "Tatooine", "Mustafar",
-    "Mandalore", "Earth", "Nal Hutta", "Corellia", "Moon", "Mars",
-    "Alderaan", "Endor", "Ahch-To", "Omicron Persei 8", "Wormulon", "Xenotaph 3",
-    "Igloopiter", "Mercury", "Venus", "Jupiter", "Saturn", "Uranus",
-    "Neptune", "Amateru", "Dagon"
-  ]
 end
