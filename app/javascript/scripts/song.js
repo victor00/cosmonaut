0e12ac64f44d8e14a57ee5536c0293ddf8e2d19c
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-02-26 21:14:18 -0300

    song.js commented

--- app/javascript/scripts/song.js
+++ app/javascript/scripts/song.js
@@ -1,12 +1,16 @@
 // success-sound
+
+// Get Params from Window where Id Search is found
 const urlParams = new URLSearchParams(window.location.search);
 
 const playSong = () => {
+	// If the div exists play a song
 	if (urlParams.get('play_song')) {
 		const sound = document.getElementById('starman');
 		sound.play();
 	}
 };
 
+// Calls the method playSong after 1 second
 setTimeout(playSong(), 1000);
 
